01fd6a406ca60ab6ed62a8516c5cd19032e6f7b9
reaper6947 <mo47da@gmail.com>
2020-12-10 14:55:18 +0600

    done

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 require("dotenv").config();
-
+const { uuid } = require("@redoxengine/medical-word-uuid");
+const {list} = require('./food-name.js');
 const { Client } = require("discord.js");
 const bot = new Client();
+const PREFIX = "$";
 
-bot.on("ready", () => console.log("bot logged in"));
+bot.on("ready", () => console.log(`${bot.user.tag} has logged in.`));
 
 bot.on("message", (msg) => {
-  if (msg.content === "ping") {
-    msg.channel.send("pong");
+  if (msg.author.bot) return;
+  if (msg.channel.name !== "random-chat") return;
+  if (msg.content.startsWith(PREFIX)) {
+    const [cmd, ...args] = msg.content
+      .trim()
+      .substring(PREFIX.length)
+      .split(/\s+/);
+    if (cmd === "ping") msg.channel.send("pong");
+    if (cmd === "rand") msg.channel.send(`${uuid(2)} ${list[Math.floor(Math.random()* list.length)]}`);
+   // if (cmd === "arr") msg.channel.send(list[Math.floor(Math.random()* list.length)]);
   }
 });
 
