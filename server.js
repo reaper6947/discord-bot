df0799ed352489a1cebd01fb55a5958291b25494
reaper6947 <mo47da@gmail.com>
2020-12-22 22:48:11 +0600

    added a webserver and basic css

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const { uuid } = require("@redoxengine/medical-word-uuid");
-const {list} = require('./food-name.js');
+const http = require("http");
+const { list } = require("./food-name.js");
 const { Client } = require("discord.js");
+
 const bot = new Client();
 const PREFIX = "$";
-
+// function for bot
 bot.on("ready", () => console.log(`${bot.user.tag} has logged in.`));
-
 bot.on("message", (msg) => {
   if (msg.author.bot) return;
   if (msg.channel.name !== "random-chat") return;
@@ -16,9 +17,27 @@ bot.on("message", (msg) => {
       .substring(PREFIX.length)
       .split(/\s+/);
     if (cmd === "ping") msg.channel.send("pong");
-    if (cmd === "rand") msg.channel.send(`${uuid(2)} ${list[Math.floor(Math.random()* list.length)]}`);
-   // if (cmd === "arr") msg.channel.send(list[Math.floor(Math.random()* list.length)]);
+    if (cmd === "rand")
+      msg.channel.send(
+        `${uuid(2)} ${list[Math.floor(Math.random() * list.length)]}`
+      );
+    // if (cmd === "arr") msg.channel.send(list[Math.floor(Math.random()* list.length)]);
   }
 });
-
 bot.login(process.env.BOT_TOKEN);
+
+//function for webserver
+const getListener = (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(`<div style=' display: grid;
+  justify-content: center;
+  align-content: center;
+  height:100%;
+  font-size:20px;'><h1>${uuid(2)} ${
+    list[Math.floor(Math.random() * list.length)]
+  }</h1></div>`);
+  res.end();
+};
+const app = http.createServer(getListener);
+const PORT = 3000 || process.env.PORT;
+app.listen(PORT, () => console.log("server started in " + PORT));
